69ca7025699998e13e7f3eb23b833ea0e288e5b9
Orthodoxos Kipouridis <akis100@gmail.com>
2015-02-01 12:33:52 +0100

    properties and saved layout files

--- KoDeMat_Collaboration3D/src/main/java/kodemat/visu/appstates/CameraDisplayAppstate.java
+++ KoDeMat_Collaboration3D/src/main/java/kodemat/visu/appstates/CameraDisplayAppstate.java
@@ -1,67 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package kodemat.visu.appstates;
 
-import com.jme3.app.Application;
-import com.jme3.app.SimpleApplication;
-import com.jme3.app.state.AbstractAppState;
-import com.jme3.app.state.AppStateManager;
-import com.jme3.renderer.Camera;
-import kodemat.visudata.VisuHelper;
-import kodemat.visudata.VisuType;
-import kodemat.visudata.VisuVector3f;
-import kodemat.visudata.visuComponents.VisuComponent;
-
-/**
- *
- * @author Institute fml (TU Munich) and Institute FLW (TU Dortmund)
- */
-public class CameraDisplayAppstate extends AbstractAppState  {
-    VisuHelper helper;
-    private Camera cam;
-    private final String userId;
-    private VisuComponent cameraNode;
-
-
-
-    public CameraDisplayAppstate(VisuHelper helper,String userID) {
-        this.helper = helper;
-        userId =userID;
-    }
-    
-
-    @Override
-    public void initialize(AppStateManager stateManager, Application app) {
-        super.initialize(stateManager, app);
-    
-        createCamera(userId);
-        
-      cam = app.getCamera();
-                
-    }
-
-    
-    
-    @Override
-    public void update(float tpf) {
-        VisuVector3f camPos = new VisuVector3f(cam.getLocation().x,cam.getLocation().y,cam.getLocation().z);
-//       helper.getComponent("camera").setTranslation(camPos);
-    }
-
-    private void createCamera(String userId) {
-
-     cameraNode = helper.createComponent("camera_"+ userId);
-            cameraNode.setType(new VisuType(VisuType.MODEL, "CeMAT_Assets/Pallete/palette.j3o"));
-            cameraNode.setTranslation(new VisuVector3f(0f,0f,0f));
-    }
-
-    
-    
-}
-=======
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -97,7 +34,6 @@ public class CameraDisplayAppstate extends AbstractAppState  {
         userId =userID;
   createCamera(userId);
     }
-    
 
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
@@ -107,8 +43,6 @@ public class CameraDisplayAppstate extends AbstractAppState  {
                
     }
 
-    
-    
     @Override
     public void update(float tpf) {
    
@@ -121,15 +55,10 @@ if(timeCount>=time){
        timeCount= 0f;
 }
     }
-
     private void createCamera(String userId) {
 
      cameraNode = helper.createComponent("camera_"+ userId);
             cameraNode.setType(new VisuType(VisuType.MODEL, "CeMAT_Assets/Pallete/palette.j3o"));
             cameraNode.setTranslation(new VisuVector3f(0f,0f,0f));
     }
-
-    
-    
 }
->>>>>>> origin/master
